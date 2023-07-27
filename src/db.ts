2aa493f16d4d9503bff53c9f746852dd746c49ea
Jonathan Mumm <jon@explorers.club>
2023-07-26 20:41:15 -0700

    remove node modules

--- src/db.ts
+++ src/db.ts
@@ -1,19 +1,19 @@
-import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
-import { migrate } from 'drizzle-orm/postgres-js/migrator';
-import postgres from 'postgres';
-import { users, sessions, responses } from './schema';
+import { drizzle, PostgresJsDatabase } from "drizzle-orm/postgres-js";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
+import postgres from "postgres";
+import { users, sessions, responses } from "./schema";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // your actual PostgreSQL connection string
-const connectionString = "postgres://postgres:adminadmin@0.0.0.0:5432/db";
+const connectionString =
+  process.env.DB_CONNECTION || "postgres://postgres:password@localhost:5432";
 
-// for migrations
-const migrationClient = postgres(connectionString, { max: 1 });
-migrate(drizzle(migrationClient), users, sessions, responses);
- 
 // for query purposes
 const queryClient = postgres(connectionString);
 const db: PostgresJsDatabase = drizzle(queryClient);
 
 // now you can use db to perform queries
 // e.g., await db.select().from(users)...
-export default db
+export default db;
