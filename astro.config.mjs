f1dcd4df5254e5b495b5cdf6fd062c27a2683ccf
Jonathan Mumm <jon@explorers.club>
2023-09-20 07:37:28 -0700

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: node({
-    mode: "standalone",
+    mode: "standalone"
   }),
-  integrations: [react()],
+  integrations: [react(), tailwind()],
   server: {
-    host: "0.0.0.0",
-  },
+    host: "0.0.0.0"
+  }
 });
