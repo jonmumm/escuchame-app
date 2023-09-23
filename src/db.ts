8e0313e01d739dc9d02eadb72068b2ef24a248b0
Jonathan Mumm <jon@explorers.club>
2023-09-23 15:23:56 -0700

    logs

--- src/db.ts
+++ src/db.ts
@@ -2,20 +2,16 @@ import dotenv from "dotenv";
 import { drizzle, type PostgresJsDatabase } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 
-console.log("HI!!!");
-
 dotenv.config();
 
 // your actual PostgreSQL connection string
 const connectionString = process.env.DATABASE_URL!;
 
-console.log({ connectionString });
-
 // for query purposes
 const queryClient = postgres(connectionString);
-console.log({ queryClient });
 const db: PostgresJsDatabase = drizzle(queryClient);
-console.log({ db });
+
+console.log("initialized db");
 
 // now you can use db to perform queries
 // e.g., await db.select().from(users)...
